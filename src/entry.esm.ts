3013b3749dd5a08300c3274ee67292340e3accad
Alexandru Ghiura <aghiura@gmail.com>
2021-04-30 12:59:50 +0300

    Export hooks

--- src/entry.esm.ts
+++ src/entry.esm.ts
@@ -16,4 +16,4 @@ export default install
 // To allow individual component use, export components
 // each can be registered via Vue.component()
 export * from '@/components'
-// export * from '@/hooks'
+export * from '@/hooks'
