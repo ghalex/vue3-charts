af542cc9362e1e6609b46be313e1999ced04833a
Dieter Späth <shad0wrunner@gmx.de>
2021-07-21 20:10:41 +0200

    Let the parent container define width and height
    With this setting the parent container is able to scale the svg while the drawing is made on a larger canvas.

--- src/components/Chart/index.vue
+++ src/components/Chart/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="chart" ref="chartEl">
     <svg
-      :width="size.width"
-      :height="size.height"
+      width="100%"
+      height="100%"
       :viewBox="`0 0 ${size.width} ${size.height}`"
       @mousemove="onMouseMove"
       @mouseleave="onMouseOut"
